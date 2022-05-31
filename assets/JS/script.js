d3293193ff9d80463c107bc52b0d382fa29fd65e
spbhanda <spbhanda@gmail.com>
2022-05-31 16:58:55 -0500

    initial setup

---
+++ assets/JS/script.js
@@ -0,0 +1,14 @@
+// Quiz questions:
+const questions = [
+   {
+      question: "Commonly used data types DO Not Include:",
+      options: ["1. <strings>", "2. <booleans>", "3. <alerts>", "4. <numbers>"],
+      answer: "3. <script>",
+   },
+
+   {
+      question: "The condition in an if/else statement is enclosed with_______.",
+      options: ["1. <quotes>", "2. <curley brackets>", "3. <paranthesis>", "4. <square brackets>"],
+      answer: "2. <script>",
+   },
+];
